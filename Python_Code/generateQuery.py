6082362615ba99b89ff582897192c35de1a1a317
poojithaamin <poojaamin87@gmail.com>
2017-05-03 14:04:16 -0700

    Added logic to filter records on search criteria

--- Python_Code/generateQuery.py
+++ Python_Code/generateQuery.py
@@ -4,20 +4,43 @@ import MySQLdb
 
 
 #Generate query 
-def generateQuery(leaf,head):
-    db = MySQLdb.connect("localhost","root","Apple@123","testdb" )
+def generateQuery(fromTable,selectColumns,whereColumns,leaf,head):
+    db = MySQLdb.connect("localhost","root","****","testdb" )
     cursor = db.cursor()
-    data=None
-    print leaf
-    print head        
-    cursor.execute("SELECT Week, Topics from testdb.CourseSchedule where Topics like ('%s%s');"%("%"+leaf+"%","%"+head+"%"))           
-    data = cursor.fetchall()
-    data = ''.join(data)
-    #data = data.translate(None, '["]')
-    print "fetch completed",data,"data"
-    if data:      
-        print "Output : %s " % data         
-    return data
+    data=None   
+    print selectColumns
+    selectString=None
+    print(len(selectColumns))
+    if len(selectColumns)>1:
+        selectString = ', '.join(str(e) for e in selectColumns) 
+        
+    else:
+        selectString= ','.join(str(s) for s in selectColumns)    
+        
+    print selectString
+    for column in set(whereColumns):
+        print column
+        cursor.execute("SELECT %s from %s where %s like ('%s%s');"%(selectString,fromTable,column,"%"+leaf+"%","%"+head+"%"))                   
+        data = cursor.fetchall()
+        print data
+        data='\n'.join(','.join(elems) for elems in data)                
+        print "fetch completed",data,"data"
+        if data:      
+            print "Output : %s " % data 
+            return data    
+    
+
+#sample input
+selectColumns=set()
+selectColumns.add("Week")  
+selectColumns.add("Dues")  
+fromTable="CourseSchedule"
+leaf="final"
+head="exam"  
+whereColumns=set()
+whereColumns.add("topics")    
+generateQuery(fromTable,selectColumns,whereColumns,leaf,head)
+
 
 
 
