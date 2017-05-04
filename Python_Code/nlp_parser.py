0502ee615b188e9b33b38eb7967169fc3cd41817
Sneha <snehaa.94@gmail.com>
2017-05-04 00:41:19 -0700

    Update nlp_parser.py

--- Python_Code/nlp_parser.py
+++ Python_Code/nlp_parser.py
@@ -2,37 +2,37 @@ import spacy
 from nltk import Tree
 from nltk.corpus import stopwords
 from file_reader import *
+from generateQuery import *
 FROM_token = ''
-SELECT_token = ''
-
+cols = list()
+CMD = ''
 nlp = spacy.load('en')
 
 def nlp_parseInput(command):
+    global CMD
+    CMD = command 
     parsedEx = nlp(command.decode('utf-8'))
     tokens = lemmatizeTokens(parsedEx)
     removeStopWords(tokens)
 
 def removeStopWords(tokens):
-    global FROM_token,SELECT_token
+    global FROM_token,CMD
     STOPLIST = set(stopwords.words('english'))
     #customize stopwords to add/remove words
-    QUE_LIST = ['what','when','who','where']
+    QUE_LIST = ['what','when','who','where','how']
     STOPLIST = [q for q in STOPLIST if q not in QUE_LIST]
     STOPLIST.append('?')
     tokens_filtered = [tok for tok in tokens if tok not in STOPLIST]
-    print "----after stop word filter---"
-    print tokens_filtered
     FROM_token = findTableorCol(tokens_filtered,"table")
     #if no table found, do not search for cols
     if(FROM_token != None):
-        SELECT_token = findTableorCol(tokens_filtered,"columns")
-        if(SELECT_token == None):
-            SELECT_token ='*'
-    print FROM_token,SELECT_token
-    
+        cols = findTableorCol(tokens_filtered,"columns")
+        if(len(cols) == 0):
+            cols.append("*")
     #new sentence without stop words
-    new_sentence = ' '.join(tokens_filtered)
-    #dependecyParse(new_sentence)
+    #new_sentence = ' '.join(tokens_filtered)
+    print FROM_token,cols
+    dependecyParse(CMD)
 
 def lemmatizeTokens(tokens):
     lemmas = []
@@ -43,20 +43,24 @@ def lemmatizeTokens(tokens):
 
 #call file_reader module to find tablename & column name
 def findTableorCol(tokens_filtered,key):
+    global cols
     if(key == "table"):
         for t in tokens_filtered:
             table_name = searchDictionary(tableDict,str(t))
+            #read columns only when we know the table
             if(table_name != None):
-                FROM_token = table_name                
+                FROM_token = table_name.strip("['']")
+                readFromFile(FROM_token,"columns")
                 return FROM_token
-    #assuming one column for noe
+    #add columns to list
     elif (key == "columns"):
         for t in tokens_filtered:
             col_name = searchDictionary(columnDict,str(t))
-            if(col_name == None):
-                SELECT_token = col_name
-                return SELECT_token
-
+            if(col_name != None):
+                cols.append(col_name.strip("['']"))
+                #SELECT_token = col_name.strip("['']")
+                #return SELECT_token
+        return cols
 
 def to_nltk_tree(node):
     if node.n_lefts + node.n_rights > 0:
@@ -65,16 +69,19 @@ def to_nltk_tree(node):
         return node.orth_
 
 #find dependency between words
-def dependecyParse(tokens_new):
-    #tokenizeText(parsedEx)
-    parseDep = nlp(tokens_new.decode('utf-8'))
+def dependecyParse(command):
+    parseDep = nlp(command.decode('utf-8'))
+    #print command
     print "---dependency tree----"
     [to_nltk_tree(sent.root).pretty_print() for sent in parseDep.sents] 
     for token in parseDep:            
         #print(token.orth_.encode('utf-8'), token.dep_.encode('utf-8'), token.head.orth_.encode('utf-8'), [t.orth_.encode('utf-8') for t in token.lefts], [t.orth_.encode('utf-8') for t in token.rights])                
-        print token.orth_.encode('utf-8'),token.head.orth_.encode('utf-8')      
+        #print token.orth_.encode('utf-8'),token.head.orth_.encode('utf-8')      
         leaf= str(token.orth_.encode('utf-8'))
-        head=str(token.head.orth_.encode('utf-8')) 
+        head=str(token.head.orth_.encode('utf-8'))
+        #call generateQuery module
+        generateQuery(FROM_token,cols,leaf,head)
+        #print leaf, head
    
 #method to find nouns, verbs ..
 def process_command(command):
@@ -100,12 +107,6 @@ def process_command(command):
                     numArr.append(token)
                 elif "ADV" in token.pos_:
                     AdverbArr.append(token)
-    # print "---nouns----"
-    # print nounArr
-    # print "---verbs----"
-    # print verbArr
-    # print "--Adverbs---"
-    # print AdverbArr
     response = "Sure...write some more code then I can do that!"
     #else:
         #response = "I am CMPE bot. How can I help you? Use the *" + EXAMPLE_COMMAND + \
