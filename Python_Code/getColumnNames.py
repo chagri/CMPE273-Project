50d7fe3a7696086722a696e4a3a8ccd08074ffda
poojithaamin <poojaamin87@gmail.com>
2017-05-04 16:08:17 -0700

    Update getColumnNames.py

--- Python_Code/getColumnNames.py
+++ Python_Code/getColumnNames.py
@@ -7,7 +7,7 @@ import MySQLdb
 def getAllColumns(table):   
     allColumns = set()
     #global allColumns
-    db = MySQLdb.connect("localhost","root","****","testdb" )
+    db = MySQLdb.connect("localhost","root","Apple@123","testdb" )
     cursor = db.cursor()
     sql = """ SHOW COLUMNS FROM %s """ %table
     cursor.execute(sql)
@@ -20,14 +20,16 @@ def getAllColumns(table):
     return allColumns
 
 def eliminateSelectColumns(selectColumns,allColumns):
+    allColumns=set(allColumns)
+    selectColumns=set(selectColumns)
     whereColumns = allColumns - selectColumns
     return whereColumns
 
 #Sample Input
-table="CourseSchedule"
-allColumns=getAllColumns(table)    
-selectColumns=set()
-selectColumns.add("Week")
-whereColumns=eliminateSelectColumns(selectColumns,allColumns)
-print whereColumns
+#table="CourseSchedule"
+#allColumns=getAllColumns(table)    
+#selectColumns=set()
+#selectColumns.add("Week")
+#whereColumns=eliminateSelectColumns(selectColumns,allColumns)
+#print whereColumns
 
