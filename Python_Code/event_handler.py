a6781bbabef0805170af6aac38b46cf9ad7b667d
poojithaamin <poojaamin87@gmail.com>
2017-05-04 16:17:53 -0700

    Update event_handler.py

--- Python_Code/event_handler.py
+++ Python_Code/event_handler.py
@@ -4,17 +4,20 @@ from slackclient import SlackClient
 from file_reader import *
 from nlp_parser import *
 
+
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 BOT_ID = os.environ.get("BOT_ID")
 AT_BOT = "<@" + BOT_ID + ">"
-db = MySQLdb.connect("localhost","root","******","testdb" )
+db = MySQLdb.connect("localhost","root","Apple@123","testdb" )
 cursor = db.cursor()
 
 
 def handle_command(command, channel):
-    #call to nlp module
+    #call to nlp module    
     response = ''
-    nlp_parseInput(command)
+    response = nlp_parseInput(command)    
+    if not response:
+        response = "Not sure what you mean. Try re-phrasing your question"
     slack_client.api_call("chat.postMessage", channel=channel,
                           text=response, as_user=True)
     
@@ -33,12 +36,12 @@ if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
         print "StarterBot connected and running!"
-        #load 'table' dictionary
-        createDictionary()  
+        #load 'table' dictionary        
+        createDictionary()          
         while True:
             command, channel = parse_slack_output(slack_client.rtm_read())
             if command and channel:
                 handle_command(command, channel)
             time.sleep(READ_WEBSOCKET_DELAY)            
     else:
-print "Connection failed. Invalid Slack token or bot ID?"
+        print "Connection failed. Invalid Slack token or bot ID?"
