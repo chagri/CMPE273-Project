9acbc12c9710f9248901ae3eaa31bea034e1b8f4
Sneha <snehaa.94@gmail.com>
2017-05-04 00:21:57 -0700

    Update file_reader.py

--- Python_Code/file_reader.py
+++ Python_Code/file_reader.py
@@ -4,24 +4,27 @@ columnDict = {}
 
 #test paths
 path_tables = '/home/Documents/tables.txt'
-path_columns = '/home/Documents/columns.txt'
-
+path_columns = '/home/Documents/Project/'
 
 def createDictionary():
-    readFromFile(path_tables, "tables")   
-    readFromFile(path_columns, "columns")
-
+    readFromFile(path_tables,"tables")
+   
 #reads from text file and create dictionaries
 def readFromFile(filePath, category):
+    if(category == "columns"):
+        #build path for fetching columns
+        filePath = path_columns + filePath +"/columns.txt"
     try:
         infile = open(filePath,'r')
         for line in infile:
             line = line.strip()
+            name = str(line.split(";")[0:1])
             parts = [p.strip() for p in line.split(",")]
             if(category == "tables"):
-                tableDict[parts[0]] = (parts[1:])
+                tableDict[name] = (parts[1:])
             elif (category == "columns"):
-                columnDict[parts[0]] = (parts[1:])
+                #columnDict[parts[0]] = (parts[1:])
+                columnDict[name] = (parts[1:])
     except IOError:
         print "Could not read file:", filePath
 
