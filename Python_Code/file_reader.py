2c543f5f6df70d2824080e3f5e4b7b8886095dc9
poojithaamin <poojaamin87@gmail.com>
2017-05-04 16:15:09 -0700

    Update file_reader.py

--- Python_Code/file_reader.py
+++ Python_Code/file_reader.py
@@ -1,27 +1,27 @@
-
 tableDict = {}
 columnDict = {}
 
 #test paths
-path_tables = '/home/Documents/tables.txt'
-path_columns = '/home/Documents/Project/'
+path_tables = '/home/poojitha/Documents/tables.txt'
+path_columns = '/home/poojitha/Documents/Project/'
 
-def createDictionary():
+def createDictionary():    
     readFromFile(path_tables,"tables")
    
 #reads from text file and create dictionaries
-def readFromFile(filePath, category):
+def readFromFile(filePath, category):    
     if(category == "columns"):
         #build path for fetching columns
         filePath = path_columns + filePath +"/columns.txt"
     try:
         infile = open(filePath,'r')
         for line in infile:
-            line = line.strip()
+            line = line.strip()            
             name = str(line.split(";")[0:1])
-            parts = [p.strip() for p in line.split(",")]
+            line = str(line.split(";")[1])
+            parts = [p.strip() for p in line.split(",")]            
             if(category == "tables"):
-                tableDict[name] = (parts[1:])
+                tableDict[name] = (parts[0:])                
             elif (category == "columns"):
                 #columnDict[parts[0]] = (parts[1:])
                 columnDict[name] = (parts[1:])
@@ -32,4 +32,4 @@ def readFromFile(filePath, category):
 def searchDictionary(dict_name,word_input):
     for k, v in dict_name.iteritems():
        if word_input in v:
-           return k
+          return k
