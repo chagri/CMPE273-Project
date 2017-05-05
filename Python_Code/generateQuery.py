fa84a120f7188af6ef2ea217604325daad892daf
poojithaamin <poojaamin87@gmail.com>
2017-05-05 14:45:20 -0700

    Update generateQuery.py

--- Python_Code/generateQuery.py
+++ Python_Code/generateQuery.py
@@ -2,49 +2,37 @@ import time
 import os
 import MySQLdb
 from getColumnNames import *
+from dateutil import parser
+from datetime import datetime
+import datetime
 
 #Generate query 
-def generateQuery(fromTable,selectColumns,searchCondition):
-
+def generateQuery(fromTable,selectColumns,searchCondition,subjectcode, sectionname,sectionperiod):
+    subjectcode = int(filter(str.isdigit, str(subjectcode)))
     allColumns = getAllColumns(fromTable)
     selectColumns = set(selectColumns)       
     whereColumns = eliminateSelectColumns(selectColumns,allColumns)       
     db = MySQLdb.connect("localhost","root","Apple@123","testdb" )
     cursor = db.cursor()
     data=None       
-    selectString=None    
+    selectString=None        
     if len(selectColumns)>1:
         selectString = ', '.join(str(e) for e in selectColumns) 
         
     else:
         selectString= ','.join(str(s) for s in selectColumns)    
-        
-    print "selectString",selectString
-    for column in set(whereColumns):
-        print column
-        print "searchCondition", searchCondition
-        rows_affected = cursor.execute("SELECT %s from %s where %s like ('%s');"%(selectString,fromTable,column,searchCondition))                   
-        data = cursor.fetchall()
+            
+    for column in set(whereColumns):                        
+        rows_affected = cursor.execute("SELECT %s from %s where %s like ('%s') and subject_code=trim('%s') and section_name=trim('%s') and section_period=trim('%s');"%(selectString,fromTable,column,searchCondition,subjectcode, sectionname,sectionperiod))                   
+        data = cursor.fetchall()        
         if rows_affected >1:                                  
-            cursor.execute("SELECT %s,%s from %s where %s like ('%s');"%(column,selectString,fromTable,column,searchCondition))                           
+            cursor.execute("SELECT %s,%s from %s where %s like ('%s') and subject_code=trim('%s') and section_name=trim('%s') and section_period=trim('%s');"%(column,selectString,fromTable,column,searchCondition,subjectcode,sectionname,sectionperiod))                           
             data = cursor.fetchall()
-        data='\n'.join(','.join(elems) for elems in data)                        
+                  
+        data='\n'.join(''.join(str(elems)) for elems in data)    
+        data = data.replace("'","").replace('(', '').replace(')', '').replace('datetime.date', '')
         if data:      
             print "Output : %s " % data 
-            return data    
+            return data
     return None
-
-#sample input
-#selectColumns=set()
-#selectColumns.add("Week")  
-##selectColumns.add("Dues")  
-#fromTable="CourseSchedule"
-#leaf="final"
-#head="exam"  
-#whereColumns=set()
-#whereColumns.add("topics")    
-#generateQuery(fromTable,selectColumns,whereColumns,leaf,head)
-
-
-
 
