1fd2680a27b9caab73c72f3cc7c8735f458f8991
poojithaamin <poojaamin87@gmail.com>
2017-05-07 00:49:51 -0700

    Update generateQuery.py

--- Python_Code/generateQuery.py
+++ Python_Code/generateQuery.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import datetime
 
 #Generate query 
-def generateQuery(fromTable,selectColumns,searchCondition,subjectcode, sectionname,sectionperiod):
+def generateQueryOne(fromTable,selectColumns,searchCondition,subjectcode, sectionname,sectionperiod):
     subjectcode = int(filter(str.isdigit, str(subjectcode)))
     allColumns = getAllColumns(fromTable)
     selectColumns = set(selectColumns)       
@@ -21,8 +21,10 @@ def generateQuery(fromTable,selectColumns,searchCondition,subjectcode, sectionna
         
     else:
         selectString= ','.join(str(s) for s in selectColumns)    
-            
-    for column in set(whereColumns):                                
+    print "searchCondition",searchCondition 
+    print "selectString",selectString       
+    for column in set(whereColumns):  
+        print "column",column          
         rows_affected = cursor.execute("SELECT %s from %s where %s like ('%s') and subject_code=trim('%s') and section_name=trim('%s') and section_period=trim('%s');"%(selectString,fromTable,column,searchCondition,subjectcode, sectionname,sectionperiod))                   
         data = cursor.fetchall()        
         if rows_affected >1:                                  
@@ -31,13 +33,43 @@ def generateQuery(fromTable,selectColumns,searchCondition,subjectcode, sectionna
                   
         data='\n'.join(''.join(str(elems)) for elems in data)    
         data = data.replace("'","").replace('(', '').replace(')', '').replace('datetime.date', '')
-
-        if data:    
-            print "column",column  
+          
+        if data:                 
             if  "date" in selectColumns:
                 data=data.replace(',','-')
                 data=data.rstrip('-')
             print "Output : %s " % data 
-            return data
+            return data 
     return None
 
+
+def generateQueryTwo(fromTable,selectColumns,subjectcode, sectionname,sectionperiod):
+    subjectcode = int(filter(str.isdigit, str(subjectcode)))
+    allColumns = getAllColumns(fromTable)
+    selectColumns = set(selectColumns)                
+    db = MySQLdb.connect("localhost","root","Apple@123","testdb" )
+    cursor = db.cursor()
+    data=None       
+    selectString=None        
+    if len(selectColumns)>1:
+        selectString = ', '.join(str(e) for e in selectColumns) 
+        
+    else:
+        selectString= ','.join(str(s) for s in selectColumns)        
+    print "selectString",selectString                 
+    rows_affected = cursor.execute("SELECT %s from %s where subject_code=trim('%s') and section_name=trim('%s') and section_period=trim('%s');"%(selectString,fromTable,subjectcode, sectionname,sectionperiod))                   
+    data = cursor.fetchall()        
+    if rows_affected >1:                                  
+         cursor.execute("SELECT %s,%s from %s where subject_code=trim('%s') and section_name=trim('%s') and section_period=trim('%s');"%(column,selectString,fromTable,subjectcode,sectionname,sectionperiod))                           
+         data = cursor.fetchall()
+                  
+    data='\n'.join(''.join(str(elems)) for elems in data)    
+    data = data.replace("'","").replace('(', '').replace(')', '').replace('datetime.date', '')
+          
+    if data:                 
+        if  "date" in selectColumns:
+                data=data.replace(',','-')
+                data=data.rstrip('-')
+        print "Output : %s " % data 
+        return data
+    return None
