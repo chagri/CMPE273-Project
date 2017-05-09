56e7b2cc66bbc0b033a1e0bd2c860f56af77c11b
Sneha <snehaa.94@gmail.com>
2017-05-09 15:50:07 -0700

    Update generateQuery.py

--- Python_Code/generateQuery.py
+++ Python_Code/generateQuery.py
@@ -10,9 +10,10 @@ import datetime
 def generateQueryOne(fromTable,selectColumns,searchCondition,subjectcode, sectionname,sectionperiod):
     #subjectcode = int(filter(str.isdigit, str(subjectcode)))
     allColumns = getAllColumns(fromTable)
+    #print searchCondition
     selectColumns = set(selectColumns)       
     whereColumns = eliminateSelectColumns(selectColumns,allColumns)       
-    db = MySQLdb.connect("localhost","root","Apple@123","testdb" )
+    db = MySQLdb.connect("localhost","root","*****","273bOT" )
     cursor = db.cursor()
     data=None       
     selectString=None        
@@ -21,12 +22,15 @@ def generateQueryOne(fromTable,selectColumns,searchCondition,subjectcode, sectio
         
     else:
         selectString= ','.join(str(s) for s in selectColumns)   
-    print "selectColumns",selectColumns               
-    for column in set(whereColumns):                 
-        rows_affected = cursor.execute("SELECT %s from %s where %s like ('%s') and subject_code=trim('%s') and section_name=trim('%s') and section_period=trim('%s');"%(selectString,fromTable,column,searchCondition,subjectcode, sectionname,sectionperiod))                   
+    print "selectColumns",selectColumns    
+    #print "wherecol",whereColumns         
+    for column in set(whereColumns):     
+        #print column            
+        #print "SELECT %s from %s where replace(%s,' ','') like ('%s') and subject_code=trim('%s') and section_name=trim('%s') and subject_term=trim('%s');"%(selectString,fromTable,column,searchCondition,subjectcode, sectionname,sectionperiod)
+        rows_affected = cursor.execute("SELECT %s from %s where replace(%s,' ','') like ('%s') and subject_code=trim('%s') and section_name=trim('%s') and subject_term=trim('%s');"%(selectString,fromTable,column,searchCondition,subjectcode, sectionname,sectionperiod))                   
         data = cursor.fetchall()        
         if rows_affected >1:                                  
-            cursor.execute("SELECT %s,%s from %s where %s like ('%s') and subject_code=trim('%s') and section_name=trim('%s') and section_period=trim('%s');"%(column,selectString,fromTable,column,searchCondition,subjectcode,sectionname,sectionperiod))                           
+            cursor.execute("SELECT %s,%s from %s where replace(%s,' ','') like ('%s') and subject_code=trim('%s') and section_name=trim('%s') and subject_term=trim('%s');"%(column,selectString,fromTable,column,searchCondition,subjectcode,sectionname,sectionperiod))                           
             data = cursor.fetchall()
                   
         data='\n'.join(''.join(str(elems)) for elems in data)    
@@ -37,15 +41,17 @@ def generateQueryOne(fromTable,selectColumns,searchCondition,subjectcode, sectio
                 data=data.replace(',','-')
                 data=data.rstrip('-')
             print "Output : %s " % data 
+            data = data.rstrip(',')
             return data 
     return None
 
 
 def generateQueryTwo(fromTable,selectColumns,subjectcode, sectionname,sectionperiod):
-    #subjectcode = int(filter(str.isdigit, str(subjectcode)))
+    #subjectcode = int(filter(str.isdigit, str(subjectcode)))   
     allColumns = getAllColumns(fromTable)
-    selectColumns = set(selectColumns)                
-    db = MySQLdb.connect("localhost","root","Apple@123","testdb" )
+    selectColumns = set(selectColumns)        
+    print "all cols", allColumns        
+    db = MySQLdb.connect("localhost","root","*****","273bOT" )
     cursor = db.cursor()
     data=None       
     selectString=None        
@@ -53,8 +59,10 @@ def generateQueryTwo(fromTable,selectColumns,subjectcode, sectionname,sectionper
         selectString = ', '.join(str(e) for e in selectColumns) 
         
     else:
-        selectString= ','.join(str(s) for s in selectColumns)                      
-    rows_affected = cursor.execute("SELECT %s from %s where subject_code=trim('%s') and section_name=trim('%s') and section_period=trim('%s');"%(selectString,fromTable,subjectcode, sectionname,sectionperiod))                   
+        selectString= ','.join(str(s) for s in selectColumns)  
+    #print "SELECT %s from %s where subject_code=trim('%s') and section_name=trim('%s') and subject_term=trim('%s');"%(selectString,fromTable,subjectcode, sectionname,sectionperiod)
+
+    rows_affected = cursor.execute("SELECT %s from %s where subject_code=trim('%s') and section_name=trim('%s') and subject_term=trim('%s');"%(selectString,fromTable,subjectcode, sectionname,sectionperiod))                   
     data = cursor.fetchall()        
     data='\n'.join(''.join(str(elems)) for elems in data)    
     data = data.replace("'","").replace('(', '').replace(')', '').replace('datetime.date', '')
@@ -64,5 +72,6 @@ def generateQueryTwo(fromTable,selectColumns,subjectcode, sectionname,sectionper
                 data=data.replace(',','-')
                 data=data.rstrip('-')
         print "Output : %s " % data 
+        data = data.rstrip(',')
         return data
     return None
