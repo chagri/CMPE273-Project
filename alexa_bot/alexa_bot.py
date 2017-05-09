67a1cb811c533467499763e650342398b3f02c93
sur <surbhi.rjain28@gmail.com>
2017-05-08 22:29:14 -0700

    Adding start skill for alexa_bot

--- alexa_bot/alexa_bot.py
+++ alexa_bot/alexa_bot.py
@@ -6,6 +6,20 @@ import logging
 app = Flask(__name__)
 ask = Ask(app, '/summarizer')
 
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+
+@app.route('/')
+def homepage():
+    return  'hello world!'
+
+@ask.launch
+def start_skill():
+    #welcome_message = "Hello, I am alexa bot for Enterprise Distributed Systems Course by Professor Sithu Aung. What would you like to know about?"
+    welcome_message = "hello"
+    return  question(welcome_message)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
