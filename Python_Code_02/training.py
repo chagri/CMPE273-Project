f7f690e4d6fe6e2a6339814c52928c55b9ce792b
tanayr03 <tanayr03@gmail.com>
2017-05-10 16:32:08 -0700

    Saving whole list of words to be trained together

--- Python_Code_02/training.py
+++ Python_Code_02/training.py
@@ -3,15 +3,14 @@ import os
 
 
 def saveWordsToFile(list):
-    with open("trainingData.txt", "r+") as f:
-        fileContent = f.read()
+        with open("trainingData.txt", "r+") as f:
+            fileContent = f.read()
             jsonDataDict = {}
             if os.path.getsize("trainingData.txt") > 0:
                 jsonDataDict = json.loads(fileContent)
-    
-        for word in list:
-            jsonDataDict[word] = "toBeTrained"
-            
+
+            jsonDataDict[str(list)] = "toBeTrained"
+
             f.seek(0)
             json_data = json.dumps(jsonDataDict, encoding='ascii')
             f.write(json_data)
