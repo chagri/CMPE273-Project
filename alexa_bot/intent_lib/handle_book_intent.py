10b028a671cd4dd70cdefc3c130a6341f3bc9c30
sur <surbhi.rjain28@gmail.com>
2017-05-10 01:43:50 -0700

    Adding handler for Book Intent

---
+++ alexa_bot/intent_lib/handle_book_intent.py
@@ -0,0 +1,9 @@
+
+
+def get_book_intent(phrase):
+    if "recommended book" in phrase or 'recommended book for the course' in phrase or 'which book should I refer' in phrase:
+        output = "First, Web Services, by Gustavo Alonso, Fabio Casati, Harumi Kuno and Vijay Machiraju. Second, Enterprise Integration Patterns, by Gregor Hohpe and Bobby Woolf. Third, Restful Web Services, by Leonard Richardson, Sam Ruby and David Hansson. Would you like to ask something else?"
+        return output
+    else:
+        output = "I did not understand what you are saying. Good bye. Have a nice day."
+        return output
