7b28b9e2a9788616a5e59361c7eaeeca9501c20c
sur <surbhi.rjain28@gmail.com>
2017-05-10 01:45:16 -0700

    Adding handler for help Intent

---
+++ alexa_bot/intent_lib/handle_help_intent.py
@@ -0,0 +1,7 @@
+
+def get_help_response():
+
+    return_msg = 'I can help you with anything about Enterprise Distributed Course Details. Currently, I support instructor details, \
+    course topics,  exam details, weightage details etc.. Additionaly, I can also summarize any given topic for you. Just say explain \
+    distributed systems ?'
+    return return_msg
