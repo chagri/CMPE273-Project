eebe3eaec6704b593bb58a38cabab54b84dc5d43
tanayr03 <tanayr03@gmail.com>
2017-05-10 16:22:19 -0700

    Added files to store training words

---
+++ Python_Code_02/training.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+
+def saveWordsToFile(list):
+    with open("trainingData.txt", "r+") as f:
+        fileContent = f.read()
+            jsonDataDict = {}
+            if os.path.getsize("trainingData.txt") > 0:
+                jsonDataDict = json.loads(fileContent)
+    
+        for word in list:
+            jsonDataDict[word] = "toBeTrained"
+            
+            f.seek(0)
+            json_data = json.dumps(jsonDataDict, encoding='ascii')
+            f.write(json_data)
+            f.truncate()
+
