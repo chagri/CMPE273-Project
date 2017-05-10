bc28a7c4e8ad25a0d88b031c9a70a4ae526db068
shivamgupta01 <shivamgupta01@gmail.com>
2017-05-09 21:58:04 -0700

    Adding Module

--- Python_Code_02/greensheet_response.py
+++ Python_Code_02/greensheet_response.py
@@ -3,7 +3,7 @@ from SpellCheck import SpellCheckResponse
 import MySQLdb
 import enchant
 from checkGreensheetAttribute import checkSubjectCode
-import tanayTest
+import getColumnandTableName
 
 d = enchant.Dict("en_US")
 nlp = spacy.load('en')
@@ -39,26 +39,34 @@ def DB_Response(command):
     checkGreensheet =  checkSubjectCode(mainAttributes)
     if checkGreensheet == False:
         return "Hello There I cannot find the GreenSheet you are seeking for try seaching for someother GreenSheet."
-    tempColoumn,table = tanayTest.getColumnName(responseAttributes)
-    print tempColoumn
+
+
+
+
+
 
     for word in responseAttributes:
            print  "Checking spell check response"
            if SpellCheckResponse(word) == False:
             print "found an spelling error"
-            return "Hey I see There Is someting wrong with the Spelling you provided. Do you mean" +  str(d.suggest(word)) + "  instead of "+ str(word)
+            return "Hey I see There Is something wrong with the Spelling you provided. Do you mean" +  str(d.suggest(word)) + "  instead of "+ str(word)
+
 
 
     if flag == 1:
         print "Finally got every thing All right"
+        tempColoumn, table = getColumnandTableName.getColumnAndTableName(responseAttributes)
+        if tempColoumn == None:
+            return "Hey I cannot fetch the result for your query, please try re-phrashing the question"
+        print tempColoumn
         query = "SELECT " + str(tempColoumn) + " FROM " + str(table) +" where subject_code=" + "'" + str(mainAttributes[0]) + "' and section_name =" + "'" + str(mainAttributes[1]) + "' and subject_term=" + "'" + str(mainAttributes[2]) + "';"
         print query
         cur.execute(query)
         response = cur.fetchall()
-        return str(response)
+        return str(response).replace("(","").replace(")","")
     else:
         print "Test from Hello"
-        response = 'hey there, Try asking me something from your greesheet For Example: cmpe273 section2 spring,2017, who is the instructor?'
+        response = 'Hello There there, Try asking me something from your greesheet For Example: cmpe273 section2 spring,2017, who is the instructor?'
         return response
 
     db.close()
